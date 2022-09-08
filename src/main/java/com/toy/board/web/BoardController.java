f96a25b1776a62eec6b8fba0c74bae6992d3175d
JCY <jcy8206@gmail.com>
2022-09-08 13:59:42 +0900

    ãëform update

--- src/main/java/com/toy/board/web/BoardController.java
+++ src/main/java/com/toy/board/web/BoardController.java
@@ -1,15 +1,56 @@
 package com.toy.board.web;
 
+import com.toy.board.domain.Board;
+import com.toy.board.reopsitory.BoardRepository;
+import com.toy.board.validator.BoardValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
-    public String list() {
-        return "board/list";
+    public String list(Model model) {
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards", boards);
+        return "/board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+
+        if (id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            Optional<Board> board = boardRepository.findById(id);
+            model.addAttribute("board", board);
+        }
+        return "/board/form";
+    }
+
+    @PostMapping("/form")
+    public String form(@Valid Board board, BindingResult bindingResult) {
+
+        boardValidator.validate(board, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
